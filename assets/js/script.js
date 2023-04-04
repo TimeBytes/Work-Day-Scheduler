0298ae8447b4c22520d52d3bef2066176b0a61cd
TimeBytes <123673532+TimeBytes@users.noreply.github.com>
2023-04-04 18:31:27 -0400

    updated dayjs CDN and updated jc code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+var localStorageObject = {};
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,16 +10,39 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  $(".saveBtn").on("click", function (event) {
+    var btnClicked = $(this).parent().children("textarea").val();
+    var idOfBtnClicked = $(this).parent().attr("id");
+    localStorageObject[idOfBtnClicked] = btnClicked;
+    localStorage.setItem("DaySchedule", JSON.stringify(localStorageObject));
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  for (let i = 9; i < 18; i++) {
+    if (dayjs().format("HH") > i) {
+      $("#hour-" + i).addClass("past");
+    } else if (dayjs().format("HH") == i) {
+      $("#hour-" + i).addClass("present");
+    } else {
+      $("#hour-" + i).addClass("future");
+    }
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  localStorageObject = JSON.parse(localStorage.getItem("DaySchedule"));
+  if (localStorageObject == null) {
+    localStorageObject = {};
+  } else {
+    Object.keys(localStorageObject).forEach(function (key) {
+      $("#" + key)
+        .children("textarea")
+        .val(localStorageObject[key]);
+    });
+  }
   // TODO: Add code to display the current date in the header of the page.
+  $("#currentDay").text(dayjs().format("dddd, MMMM, Do"));
 });
